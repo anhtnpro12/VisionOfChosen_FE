67909a0e225dfb5d5c07a435a65eae30537d1104
Trịnh Nam Anh <anhtrinhnam02@gmail.com>
2025-07-17 20:55:36 +0700

    fix

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosClient: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000",
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
